1628cf3983ad1e17736cc94d2cd8cc7933d03d83
JordanPlayz158 <JordanAdamsBusiness@outlook.com>
2022-09-16 17:40:35 -0400

    Viewing works, I hope there is a better way as process_element is awful

--- src/app.rs
+++ src/app.rs
@@ -1,30 +1,143 @@
+use std::fs::File;
+use std::io::{Read};
 use eframe::{egui, epi};
+use eframe::egui::Ui;
+use flash_lso::read::Reader;
+use flash_lso::types::{AMFVersion, Element, Header, Lso, Value};
+use substring::Substring;
 
-/// We derive Deserialize/Serialize so we can persist app state on shutdown.
-#[cfg_attr(feature = "persistence", derive(serde::Deserialize, serde::Serialize))]
-#[cfg_attr(feature = "persistence", serde(default))] // if we add new fields, give them default values when deserializing old state
-pub struct TemplateApp {
-    // Example stuff:
-    label: String,
-
-    // this how you opt-out of serialization of a member
-    #[cfg_attr(feature = "persistence", serde(skip))]
-    value: f32,
+pub enum Message {
+    FileOpen(std::path::PathBuf),
+    // Other messages
 }
 
-impl Default for TemplateApp {
+pub struct App {
+    lso: Lso,
+
+    message_channel: (
+        std::sync::mpsc::Sender<Message>,
+        std::sync::mpsc::Receiver<Message>,
+    )
+}
+
+impl Default for App {
     fn default() -> Self {
         Self {
-            // Example stuff:
-            label: "Hello World!".to_owned(),
-            value: 2.7,
+            lso: Lso { header: Header {
+                length: 0,
+                name: "Not Loaded".to_string(),
+                format_version: AMFVersion::AMF0
+            }, body: vec![] },
+            message_channel: std::sync::mpsc::channel(),
         }
     }
 }
 
-impl epi::App for TemplateApp {
-    fn name(&self) -> &str {
-        "eframe template"
+impl epi::App for App {
+    /// Called each time the UI needs repainting, which may be many times per second.
+    /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
+    fn update(&mut self, ctx: &egui::CtxRef, frame: &mut epi::Frame<'_>) {
+        // This is important, otherwise file dialog can hang
+        // and messages are not processed
+        ctx.request_repaint();
+
+        loop {
+            match self.message_channel.1.try_recv() {
+                Ok(message) => {
+                    let Message::FileOpen(path_buf) = message;
+                    let mut file = File::open(path_buf).unwrap();
+                    let mut data = Vec::new();
+
+                    let _bytes = file.read_to_end(&mut data);
+                    let reader = Reader::default().parse(&data).unwrap();
+                    self.lso = reader.1;
+
+                    println!("{:?}", self.lso.header);
+                    println!("{:?}", self.lso.body)
+                }
+                Err(_) => {
+                    break;
+                }
+            }
+        }
+
+        // Examples of how to create different panels and windows.
+        // Pick whichever suits you.
+        // Tip: a good default choice is to just keep the `CentralPanel`.
+        // For inspiration and more examples, go to https://emilk.github.io/egui
+
+        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
+            // The top panel is often a good place for a menu bar:
+            egui::menu::bar(ui, |ui| {
+                egui::menu::menu(ui, "File", |ui| {
+                    if ui.button("Open...").clicked() {
+                        let task = rfd::AsyncFileDialog::new()
+                            .add_filter("SOL files", &["sol"])
+                            .set_directory(std::env::current_dir().unwrap())
+                            .pick_file();
+
+                        let message_sender = self.message_channel.0.clone();
+
+                        execute(async move {
+                            let file = task.await;
+
+                            if let Some(file) = file {
+                                let file_path = std::path::PathBuf::from(file.path());
+                                message_sender.send(Message::FileOpen(file_path)).ok();
+                            }
+                        });
+                    }
+                    if ui.button("Exit").clicked() {
+                        frame.quit();
+                    }
+                });
+            });
+        });
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            let header = &mut self.lso.header;
+            if header.length != 0 {
+                // The central panel the region left after adding TopPanel's and SidePanel's
+                ui.heading("Header");
+
+                ui.horizontal(|ui| {
+                    ui.label("Name:");
+                    ui.text_edit_singleline(&mut header.name);
+                });
+
+                ui.horizontal(|ui| {
+                    ui.label("SOL/AMF Version:");
+                    ui.code(header.format_version);
+                });
+
+                ui.horizontal(|ui| {
+                    ui.label("Length:");
+                    ui.code(header.length);
+                });
+
+
+                ui.heading("Body");
+                let amf0 = header.format_version == AMFVersion::AMF0;
+                let body = &self.lso.body;
+
+                for element in body {
+                    process_element(ui, amf0, element)
+                }
+
+                egui::warn_if_debug_build(ui);
+            } else {
+                ui.heading("No SOL file loaded.");
+            }
+        });
+
+        /*if false {
+            egui::Window::new("Window").show(ctx, |ui| {
+                ui.label("Windows can be moved by dragging them.");
+                ui.label("They are automatically sized based on contents.");
+                ui.label("You can turn on resizing and scrolling if you like.");
+                ui.label("You would normally chose either panels OR windows.");
+            });
+        }*/
     }
 
     /// Called once before the first frame.
@@ -49,70 +162,124 @@ impl epi::App for TemplateApp {
         epi::set_value(storage, epi::APP_KEY, self);
     }
 
-    /// Called each time the UI needs repainting, which may be many times per second.
-    /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
-    fn update(&mut self, ctx: &egui::CtxRef, frame: &mut epi::Frame<'_>) {
-        let Self { label, value } = self;
+    fn name(&self) -> &str {
+        "SOL Editor"
+    }
+}
 
-        // Examples of how to create different panels and windows.
-        // Pick whichever suits you.
-        // Tip: a good default choice is to just keep the `CentralPanel`.
-        // For inspiration and more examples, go to https://emilk.github.io/egui
+fn execute<F: std::future::Future<Output = ()> + Send + 'static>(f: F) {
+    std::thread::spawn(move || {
+        futures::executor::block_on(f);
+    });
+}
 
-        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-            // The top panel is often a good place for a menu bar:
-            egui::menu::bar(ui, |ui| {
-                egui::menu::menu(ui, "File", |ui| {
-                    if ui.button("Quit").clicked() {
-                        frame.quit();
-                    }
+fn process_element(ui: &mut Ui, amf0: bool, element: &Element) {
+    let mut no_value = false;
+
+    ui.label(&element.name);
+
+    let value = &element.value();
+
+    match value {
+        Value::Number(number) => {ui.code(number)},
+        Value::Bool(bool) => {ui.code(bool)},
+        Value::String(string) => {ui.code(string)},
+        Value::Object(list_of_elements, class_definition) => {
+            for i in 0..list_of_elements.len() {
+                ui.horizontal(|ui| {
+                    ui.add_space(10.0);
+
+                    let element1 = &list_of_elements[i];
+
+                    ui.label(&element1.name);
+                    process_element(ui, amf0, &element1);
                 });
-            });
-        });
-
-        egui::SidePanel::left("side_panel").show(ctx, |ui| {
-            ui.heading("Side Panel");
-
-            ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(label);
-            });
-
-            ui.add(egui::Slider::new(value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                *value += 1.0;
             }
 
-            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
+            if class_definition.is_some() {
+                let class_definition = &class_definition.as_ref().unwrap();
+
                 ui.horizontal(|ui| {
-                    ui.spacing_mut().item_spacing.x = 0.0;
-                    ui.label("powered by ");
-                    ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-                    ui.label(" and ");
-                    ui.hyperlink_to("eframe", "https://github.com/emilk/egui/tree/master/eframe");
+                    ui.add_space(10.0);
+                    if class_definition.name.is_empty() {
+                        ui.label("Class Definition: \"\"");
+                    } else {
+                        ui.label("Class Definition: ");
+                    }
+
+                    ui.code(&class_definition.name);
                 });
-            });
-        });
 
-        egui::CentralPanel::default().show(ctx, |ui| {
-            // The central panel the region left after adding TopPanel's and SidePanel's
+                let static_properties = &class_definition.static_properties;
+                let mut static_properties_string = "[".to_owned();
 
-            ui.heading("eframe template");
-            ui.hyperlink("https://github.com/emilk/eframe_template");
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/master/",
-                "Source code."
-            ));
-            egui::warn_if_debug_build(ui);
-        });
+                for static_property in static_properties {
+                    static_properties_string.push_str("\"");
+                    static_properties_string.push_str(static_property);
+                    static_properties_string.push_str("\", ");
+                }
 
-        if false {
-            egui::Window::new("Window").show(ctx, |ui| {
-                ui.label("Windows can be moved by dragging them.");
-                ui.label("They are automatically sized based on contents.");
-                ui.label("You can turn on resizing and scrolling if you like.");
-                ui.label("You would normally chose either panels OR windows.");
-            });
+                let static_properties_string_length = static_properties_string.len();
+
+                if static_properties_string_length != 1 {
+                    static_properties_string = static_properties_string.substring(0, static_properties_string_length - 2).to_string();
+                }
+
+                static_properties_string.push_str("]");
+
+                ui.horizontal(|ui| {
+                    ui.add_space(20.0);
+                    ui.label("Static Properties: ");
+                    ui.code(static_properties_string);
+                }).response
+            } else {
+                ui.code("No Class Definition Found!")
+            }
+        },
+        Value::Null => {
+            ui.horizontal(|ui| {
+                ui.code("null");
+            }).response
+        },
+        Value::Undefined => {ui.code("undefined")},
+        //Value::ECMAArray(Vec<Rc<Value>>, Vec<Element>, u32),
+        //Value::StrictArray(Vec<Rc<Value>>),
+        //Value::Date(f64, Option<u16>)
+        Value::Unsupported => {ui.code("unsupported")},
+        Value::XML(string, bool) => {
+            ui.horizontal(|ui| {
+                ui.code(string);
+                ui.code(bool);
+            }).response
+        }
+        //Value::AMF3(Rc<Value>),
+        _ => {
+            no_value = true;
+            ui.code("Couldn't find type.")
+        },
+    };
+
+    if amf0 && no_value {
+        return;
+    };
+
+    let mut no_value2 = false;
+
+    match value {
+        Value::Integer(integer) => {ui.code(integer);}
+        //Value::ByteArray(Vec<u8>) => ,
+        //Value::VectorInt(_, _) => ,
+        //Value::VectorUInt(_, _) => ,
+        //Value::VectorDouble(_, _) => ,
+        //Value::VectorObject(_, _, _) => ,
+        //Value::Dictionary(_, _) => ,
+        //Value::Custom(_, _, _) => ,
+        _ => {
+            no_value2 = true;
         }
     }
+
+    if !amf0 && no_value && no_value2 {
+        ui.code("Couldn't find type.");
+    };
 }
